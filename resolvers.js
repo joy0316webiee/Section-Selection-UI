bab94d087c32b7ba4f74dbbba3ca2e08c1569ff9
joy0316webiee <joy0316webiee@gmail.com>
2019-08-07 18:15:09 +1000

    some update

--- resolvers.js
+++ resolvers.js
@@ -3,6 +3,25 @@ const puppeteer = require('puppeteer');
 exports.resolvers = {
   Query: {
     urlScreen: async (_, { url }, __) => {
+      // UPDATED BY TIGRAN - BEGIN
+      // await page.setViewport({ width: 1920, height: 1080 });
+      // await page.goto(url, { waitUntil: 'networkidle0' });
+      // await page._client.send('Animation.setPlaybackRate', { playbackRate: 2 });
+
+      // const screenHeight = await page.evaluate(() => {
+      //   return document.documentElement.offsetHeightl;
+      // });
+      // const b64string = await page.screenshot({ encoding: 'base64', fullPage: true });
+
+      // await browser.close();
+
+      // return {
+      //   screenHeight,
+      //   screenWidth: 1920,
+      //   image: b64string
+      // };
+      // UPDATED BY TIGRAN - END
+
       const browser = await puppeteer.launch();
       const page = await browser.newPage();
       await page.goto(url);
