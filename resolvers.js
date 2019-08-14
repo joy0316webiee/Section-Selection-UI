45ab7f4a08b6f398231930c08b01c96fa9989f33
joy0316webiee <joy0316webiee@gmail.com>
2019-08-14 10:09:50 +1000

    update urlScreen resolver

--- resolvers.js
+++ resolvers.js
@@ -3,40 +3,26 @@ const puppeteer = require('puppeteer');
 exports.resolvers = {
   Query: {
     urlScreen: async (_, { url }, __) => {
-      // UPDATED BY TIGRAN - BEGIN
-      // await page.setViewport({ width: 1920, height: 1080 });
-      // await page.goto(url, { waitUntil: 'networkidle0' });
-      // await page._client.send('Animation.setPlaybackRate', { playbackRate: 2 });
-
-      // const screenHeight = await page.evaluate(() => {
-      //   return document.documentElement.offsetHeightl;
-      // });
-      // const b64string = await page.screenshot({ encoding: 'base64', fullPage: true });
-
-      // await browser.close();
-
-      // return {
-      //   screenHeight,
-      //   screenWidth: 1920,
-      //   image: b64string
-      // };
-      // UPDATED BY TIGRAN - END
-
       const browser = await puppeteer.launch();
       const page = await browser.newPage();
-      await page.goto(url);
 
-      const dimensions = await page.evaluate(() => {
-        return {
-          screenWidth: document.documentElement.clientWidth,
-          screenHeight: document.documentElement.clientHeight
-        };
+      await page.setViewport({ width: 1920, height: 1080 });
+      await page.goto(url, { waitUntil: 'networkidle0' });
+      await page._client.send('Animation.setPlaybackRate', { playbackRate: 2 });
+
+      const screenHeight = await page.evaluate(() => {
+        return document.documentElement.offsetHeight;
       });
-      const b64string = await page.screenshot({ encoding: 'base64' });
+
+      const b64string = await page.screenshot({ encoding: 'base64', fullPage: true });
 
       await browser.close();
 
-      return { ...dimensions, image: b64string };
+      return {
+        screenHeight,
+        screenWidth: 1920,
+        image: b64string
+      };
     }
   },
   Mutation: {
